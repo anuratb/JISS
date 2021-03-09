26bd1fd1b9f5d3d22ce1857a488123d4420b90e9
anuratb <anuratb@yahoo.com>
2021-03-10 01:03:49 +0530

    Added Form Handlers

--- frontend/src/components/login.component.js
+++ frontend/src/components/login.component.js
@@ -1,29 +1,43 @@
 import React, { Component } from "react";
-class Welcome extends Component{
-    render(){
+class Welcome extends Component {
+    render() {
         return (
             <div ><h1>Welcome to JISS</h1></div>
         );
     }
 }
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {username:'',password:''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+    handleSubmit(event) {
+        //console.log('Submitting'); //For Debug
+        alert('A username was submitted: ' + this.state.username);
+        event.preventDefault();
+    }
     render() {
         return (
             <div>
-            <Welcome/>
-            <form>
-                <h3>Please Sign In</h3>
+                <Welcome />
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Please Sign In</h3>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter Email" />
-                </div>
-                <br/>
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-                {/*
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input type="text" name="username" onChange={this.handleChange} className="form-control" placeholder="Enter Username" />
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" onChange={this.handleChange} className="form-control" placeholder="Enter password" />
+                    </div>
+                    {/*
                 <div className="form-group">
                     <div className="custom-control custom-checkbox">
                         <input type="checkbox" className="custom-control-input" id="customCheck1" />
@@ -31,11 +45,11 @@ export default class Login extends Component {
                     </div>
                 </div>*/}
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                {/*<p className="forgot-password text-right">
+                    <button type="submit"  className="btn btn-primary btn-block">Submit</button>
+                    {/*<p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
             </p>*/}
-            </form>
+                </form>
             </div>
         );
     }
